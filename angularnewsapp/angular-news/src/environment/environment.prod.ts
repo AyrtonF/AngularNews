b11e008c5a29a0e6536a65463af72c786e0031a2
AyrtonF <ayrtonleonardo14@gmail.com>
2025-06-04 01:00:24 -0300

    iniciando frontend

---
+++ angularnewsapp/angular-news/src/environment/environment.prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyIc-i_3eYm8CP1GmC5T1kk_HzZh7EhLk",
+    authDomain: "angularnews-bbc07.firebaseapp.com",
+    projectId: "angularnews-bbc07",
+    storageBucket: "angularnews-bbc07.appspot.com",
+    messagingSenderId: "358032595791",
+    appId: "1:358032595791:web:4f341213b363839a0c033a"
+  },
+  newsApi: {
+    apiKey: "fad5abc231ea4171a40646587577fbf1", 
+    apiUrl: "https://newsapi.org/v2"
+  }
+};
